2560cb7d4af5367ac34fcee6e0b7cdbc595b4470
pashwiniis <pashwini684@gmail.com>
2021-12-09 21:58:17 +0530

    Create a sorted linked list in acending orderd

--- LinkedList.cs
+++ LinkedList.cs
@@ -153,6 +153,38 @@
             }
             Console.WriteLine("\n");
         }
+        internal void sortList()
+        {
+            //Node current will point to head  
+            Nodes current = head, index = null;
+            int temp;
+
+            if (head == null)
+            {
+                return;
+            }
+            else
+            {
+                while (current != null)
+                {
+                    //Node index will point to node next to current  
+                    index = current.next;
+
+                    while (index != null)
+                    {
+                        //If current node's data is greater than index's node data, swap the data between them  
+                        if (current.data > index.data)
+                        {
+                            temp = current.data;
+                            current.data = index.data;
+                            index.data = temp;
+                        }
+                        index = index.next;
+                    }
+                    current = current.next;
+                }
+            }
+        }
 
         internal int LinkedListSize()
         {
